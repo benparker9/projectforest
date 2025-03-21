8acffc9d18c62137c2e6b8431a27fa631ecb9ca2
benparker9 <benparker330@gmail.com>
2025-03-21 15:25:00 -0700

    Add files via upload

---
+++ pages/javapf.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {
+    const container = document.getElementById('container');
+    container.style.transition = 'left 1s ease-in-out'; // Add smooth transition
+    container.style.left = '0'; // Slide the content into vi
+})
