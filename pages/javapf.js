ca2a50bf84b6256b4d49c48158dc91c725ea6a35
benparker9 <benparker330@gmail.com>
2025-03-21 18:25:22 -0700

    Delete pages directory

--- pages/javapf.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load', function() {
-    const container = document.getElementById('container');
-    container.style.transition = 'left 1s ease-in-out'; // Add smooth transition
-    container.style.left = '0'; // Slide the content into vi
-})
