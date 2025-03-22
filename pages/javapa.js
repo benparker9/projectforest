cbb03e69540cf233724c36383834391e81c31331
benparker9 <benparker330@gmail.com>
2025-03-21 17:28:09 -0700

    Delete pages directory

--- pages/javapa.js
+++
@@ -1,9 +0,0 @@
-function toggleTable(){
-    document.getElementById("myTable").classList.toggle("climatetab")
-}
-function togglecom(){
-    document.getElementById("com1").classList.toggle("comtab")
-}
-function togglebio(){
-    document.getElementById("bio1").classList.toggle("biotab")
-}
